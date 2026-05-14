0ee962b932db73706380119688203b515483a6a7
vikaspandy75-alt <vikaspandy75@gmail.com>
2026-05-14 22:58:36 +0530

    Register service worker on window load

--- app.js
+++ app.js
@@ -194,3 +194,10 @@ font-weight:bold;"
   ],
   "configuration_version": "1"
 }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js")
+      .then(() => console.log("SW Registered"))
+      .catch(err => console.log(err));
+  });
+}
